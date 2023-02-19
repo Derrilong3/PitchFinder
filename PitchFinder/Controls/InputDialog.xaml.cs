96e299af6beffefbdc20bd33bc49c0f346d0fbe9
Derrilong3 <ivan2003.185@gmail.com>
2023-02-19 14:08:19 +0200

    Add InputDialog and ListBoxDialog controls

---
+++ PitchFinder/Controls/InputDialog.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+
+namespace PitchFinder.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для InputDialog.xaml
+    /// </summary>
+    public partial class InputDialog : Window
+    {
+        public InputDialog(string header)
+        {
+            InitializeComponent();
+            this.header.Text = header;
+        }
+
+        public string InputText
+        {
+            get { return inputText.Text; }
+        }
+
+        private void AcceptButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+    }
+}
---
+++ PitchFinder/Controls/ListBoxDialog.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace PitchFinder.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для ListBoxDialog.xaml
+    /// </summary>
+    public partial class ListBoxDialog : Window
+    {
+        public ListBoxDialog(string header, string[] names)
+        {
+            InitializeComponent();
+            listBox.ItemsSource = names;
+            this.header.Text = header;
+        }
+
+        public int SelectedIndex
+        {
+            get { return listBox.SelectedIndex; }
+        }
+
+        private void listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DialogResult = true;
+        }
+    }
+}
